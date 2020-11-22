63dddbfd67a7ac555634ad8e35b463c33dbc6adb
Leonardo Scoppitto <70918126+Skiby7@users.noreply.github.com>
2020-11-22 17:15:43 +0100

    Update somma.s
    Working assignment

--- somma.s
+++ somma.s
@@ -1,28 +1,28 @@
-		.data
-		.text
+	.data
+	.text
         .global somma
-		.type somma %function
+	.type somma %function
 		
 somma: 	push {r0} @ salvo r0	
-		push {LR} @ salvo LR prima della chiamata di strlen
-		bl strlen 
-		pop {LR} @ ripristino LR
-		mov r2, r0 @sposto in r2 la lunghezza della stringa
-		pop {r0} @ ripristino in r0 l indirizzo della stringa
-		mov r1, #0 @ pulisco r1
-		mov r3, #0 @ pulisco r3
-		mov r6, #0 @ pulisco r6 che mi servirà dopo
+	push {LR} @ salvo LR prima della chiamata di strlen
+	bl strlen 
+	pop {LR} @ ripristino LR
+	mov r2, r0 @sposto in r2 la lunghezza della stringa
+	pop {r0} @ ripristino in r0 l indirizzo della stringa
+	mov r1, #0 @ pulisco r1
+	mov r3, #0 @ pulisco r3
+	mov r6, #0 @ pulisco r6 che mi servirà dopo
 
-		@ La funzione pusho serve a salvare tutti i caratteri della stringa nello stack
+	@ La funzione pusho serve a salvare tutti i caratteri della stringa nello stack
 		
 pusho: 	cmp r3, r2 @ guardia del while(i < n) con r2 = n = strlen(string) e r3 = i
-		movge r3, #0 @ reset dei registri che mi servono dopo per la somma
-		movge r4, #0 @ r3 lo uso nella funzine quanto e r4 nel while successivo
-		bge sum @ mi sposto su somma
-		ldrb r5, [r0], #1 @ carico il carattere in r5 e lo salvo nello stack
-		push {r5}
-		add r3, r3, #1 
-		b pusho
+	movge r3, #0 @ reset dei registri che mi servono dopo per la somma
+	movge r4, #0 @ r3 lo uso nella funzine quanto e r4 nel while successivo
+	bge sum @ mi sposto su somma
+	ldrb r5, [r0], #1 @ carico il carattere in r5 e lo salvo nello stack
+	push {r5}
+	add r3, r3, #1 
+	b pusho
 
 sum:  	cmp r4, r2
         bge fine
@@ -35,6 +35,5 @@ sum:  	cmp r4, r2
         b sum
 
 fine:	mov r0, r6 @ sposto il risultato su r0 
-		mov PC, LR @ ritorno al main.c
+	mov PC, LR @ ritorno al main.c
 	
-
